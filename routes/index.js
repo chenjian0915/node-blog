d4d6627b0d091fd3bec1d162c7d513f852c85e82
chenjian <togo0915@163.com>
2017-11-15 21:54:18 +0800

    blog

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+module.exports = function(app){
+  app.get('/',function(req,res){
+    res.render(
+        'index' , {'title':'Express'}
+    )
+  });
+  app.get('/reg',function(req,res){
+    res.render(
+        'reg' , {'title':'注册'}
+    )
+  });
+  app.post('/reg',function(req,res){
+
+  });
+  app.get('/login',function(req,res){
+    res.render(
+        'login' , {'title':'登录'}
+    )
+  });
+  app.post('/login',function(req,res){
+    res.render(
+        'index' , {'title':'Express'}
+    )
+  });
+  app.get('/post',function(req,res){
+    res.render(
+        'post' , {'title':'发表'}
+    )
+  });
+  app.post('/post',function(req,res){
+
+  });
+  app.get('/logout',function(req,res){
+
+  });
+
+
+}
